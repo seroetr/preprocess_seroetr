650788614ac557556ef362ebb3dff04c447b70c8
seroetr <eemserhatdinleyen@gmail.com>
2022-08-02 15:47:40 +0300

    Added my project

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md","r") as file:
+	long_description = file.read()
+
+setuptools.setup(
+	name="preprocess_seroetr",# this should be unique
+	version = "0.0.1",
+	author = "Serhat Dinleyen",
+	author_email = "eemserhatdinleyen@gmail.com",
+	description = " This is preprocessing package",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	packages = setuptools.find_packages(),
+	classifiers = [
+	"Programming Language :: Python :: 3 ",
+	"License :: OSI Approved :: MIT License (MIT),"
+	" Operating System :: OS Independent"],
+	python_requires = ">=3.8"
+	)
